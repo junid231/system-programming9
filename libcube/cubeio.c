764159ed51f56c8dbb4a9b27d6fa5ac9d7ae4618
system <junsang2407@gmail.com>
2023-06-03 11:41:33 +0100

    input

--- libcube/cubeio.c
+++ libcube/cubeio.c
@@ -3,6 +3,15 @@
 #include "coroutine.h"
 #include "cube.h"
 
+#include <wiringPi.h>
+#include <wiringPiSPI.h>
+
+#define PL    2             // chip 1 : Digital out, LOW일때 들어오는 스위치 값 받음. 
+#define CP    11            // chip 2 : SPI CLK (SCK) : SCLK, 클럭용 핀
+#define CHANNEL     9       // chip 9 : SPI MISO : 값 읽어들이는 핀
+#define NUM_INPUTS  2       // 74HC165 모듈의 입력 개수
+#define LED_PIN_START   3   // LED 0번 핀
+
 static bool buttonState[6*9];
 static bool buttonprevState[6*9];
 
@@ -56,37 +65,37 @@ void StartCubeRoutine() {
     CubeInit();
     srand(time(NULL));
     memset(buttonprevState, 0, sizeof(bool) * 6*9);
+
+    int fd = wiringPiSPISetup(CHANNEL, 500000);
+    unsigned char inputData = 0;
+
+    // LED 핀을 출력으로 설정
+    for(int pin=0; pin<NUM_INPUTS; pin++)
+        pinMode(pin+LED_PIN_START, OUTPUT);
+
     Start();
 
     while (true) {
         CallCoroutines();
 
-        /* =============== Button Input Control =============== */
-
         // Real Hardware Version
-        /*for(int face=0; face<6; face++) {
-            for(int lednum=0; lednum<9; lednum++) {
-                if(buttonInput(face*lednum) && !buttonprevState[face*lednum])
-                    ButtonDown(face, lednum);
-                if(!buttonInput(face*lednum) && buttonprevState[face*lednum])
-                    ButtonUp(face, lednum);
+        for(int pin=0; pin<NUM_INPUTS; pin++) {
+            wiringPiSPIDataRW(CHANNEL, &inputData, sizeof(inputData));
+            if (inputData & (1 << pin)) {
+                digitalWrite(pin+LED_PIN_START, HIGH);
+                printf("a");
+            } else {
+                digitalWrite(pin+LED_PIN_START, LOW);
+                printf("b");
             }
-        }*/
+        }
 
         // Terminal Version
-        // TerminalInput();
-
-        /* ================ LED Output Control ================ */
-
-        // Real Hardware Version
-
-        // Terminal Version
-        TerminalOutput();
+        // TerminalOutput();
 
         /* ==================================================== */
 
         Update();
         usleep(deltaTime*1000);
-        // sleep((float)deltaTime/1000);
     }
 }
