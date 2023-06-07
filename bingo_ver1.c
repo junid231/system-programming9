117b458b815d885a8169586e9043d5b5177961fa
junid231 <108266380+junid231@users.noreply.github.com>
2023-06-07 17:35:08 +0900

    bingo

--- bingo_ver1.c
+++ bingo_ver1.c
@@ -17,7 +17,7 @@ static int current_bingo [6][9] =
 
 };
 static int bingo_count = 0;
-
+bool is_end = false;
 void ButtonDown(int face, int lednum)
 {
     /* ---------- 여기에 몇 번 face의 몇 번 버튼이 눌렸을 때 어떤 동작을 수행할지를 작성 ----------*/
@@ -28,13 +28,27 @@ void ButtonDown(int face, int lednum)
     if (bingo_count < 3){
         int newface = face;
         int newlednum = lednum;
-        if(lednum == 4){
-            if (GetDirLED(face, lednum, &newface, &newlednum, 0).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 2).b == 255){
+        SetColor(face, lednum, MakeColor(0, 0, 255));
+        StartFadeColorCoroutine(FadeColor, face, lednum, 4000);
+        current_bingo[face][lednum] = 1;
+        if (is_end) {
+            for(int i=0; i<6; i++){
+                for(int j=0; j<9; j++){
+                    if (current_bingo[i][j] == 1){
+                        current_bingo[i][j] = 0;
+                        SetColor(i, j, MakeColor(0, 0, 0));
+                        StartFadeColorCoroutine(FadeColor, i, j, 4000);
+                    }
+                }
+            }
+            is_end = false;
+        } else if(lednum == 4){
+            if (GetColor(face, 1).b == 255 && GetColor(face, 7).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLED(face, lednum, &newface, &newlednum, 1).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 3).b == 255){
+            if (GetColor(face, 5).b == 255 && GetColor(face, 3).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
@@ -49,58 +63,63 @@ void ButtonDown(int face, int lednum)
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 1){
-            if (GetDirLED(face, lednum, &newface, &newlednum, 1).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 3).b == 255){
+            if (GetColor(face, 0).b == 255 && GetColor(face, 2).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 2, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 2).b == 255){
+            if (GetColor(face, 7).b == 255 && GetColor(face, 4).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 7){
-            if (GetDirLED(face, lednum, &newface, &newlednum, 1).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 3).b == 255){
+            if (GetColor(face, 6).b == 255 && GetColor(face, 8).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 0, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 0).b == 255){
+            if (GetColor(face, 1).b == 255 && GetColor(face, 4).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } 
         else if(lednum == 3){
-            if (GetDirLED(face, lednum, &newface, &newlednum, 0).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 2).b == 255){
+            if (GetColor(face, 0).b == 255 && GetColor(face, 6).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 1, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 1).b == 255){
+            if (GetColor(face, 5).b == 255 && GetColor(face, 4).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 5){
-            if (GetDirLED(face, lednum, &newface, &newlednum, 0).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 2).b == 255){
+            if (GetColor(face, 3).b == 255 && GetColor(face, 4).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 3, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 3).b == 255){
+            if (GetColor(face, 2).b == 255 && GetColor(face, 8).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 0){
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 1, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 1).b == 255){
+            if (GetColor(face, 2).b == 255 && GetColor(face, 1).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 2, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 2).b == 255){
+            if (GetColor(face, 3).b == 255 && GetColor(face, 6).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
@@ -110,13 +129,14 @@ void ButtonDown(int face, int lednum)
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 2){
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 3, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 3).b == 255){
+            if (GetColor(face, 5).b == 255 && GetColor(face, 8).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 2, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 2).b == 255){
+            if (GetColor(face, 0).b == 255 && GetColor(face, 1).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
@@ -126,13 +146,14 @@ void ButtonDown(int face, int lednum)
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 6){
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 1, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 1).b == 255){
+            if (GetColor(face, 7).b == 255 && GetColor(face, 8).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 0, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 0).b == 255){
+            if (GetColor(face, 0).b == 255 && GetColor(face, 3).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
@@ -142,13 +163,14 @@ void ButtonDown(int face, int lednum)
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         } else if(lednum == 8){
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 0, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 0).b == 255){
+            if (GetColor(face, 2).b == 255 && GetColor(face, 5).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
             }
-            if (GetDirLEDwithDist(face, lednum, &newface, &newlednum, 3, 2).b == 255 && GetDirLED(face, lednum, &newface, &newlednum, 3).b == 255){
+            if (GetColor(face, 6).b == 255 && GetColor(face, 7).b == 255){
                 bingo_count++;
                 newface = face;
                 newlednum = lednum;
@@ -158,15 +180,9 @@ void ButtonDown(int face, int lednum)
                 newface = face;
                 newlednum = lednum;
             }
+            return;
         }
-        SetColor(face, lednum, MakeColor(0, 0, 255));
-        StartFadeColorCoroutine(FadeColor, face, lednum, 4000);
-        current_bingo[face][lednum] = 1;
-    } else {
-            SetColor(face, lednum, MakeColor(255, 0, 0));
-            StartFadeColorCoroutine(FadeColor, face, lednum, 4000);
-
-    }
+    } 
     
 
     // 눌린 버튼 색을 무작위로 바꾸는 애플리케이션
@@ -179,8 +195,21 @@ void ButtonDown(int face, int lednum)
 void ButtonUp(int face, int lednum)
 {
     /* --------- 여기에 몇 번 face의 몇 번 버튼이 떼어졌을 때 어떤 동작을 수행할지를 작성 ---------*/
-
+    while (bingo_count >= 3) {
+        for(int i=0; i<6; i++){
+            for(int j=0; j<9; j++){
+                if (current_bingo[i][j] == 1){
+                    SetColor(i, j, MakeColor(255, 0, 0));
+                    StartFadeColorCoroutine(FadeColor, i, j, 4000);
+                }
+            }
+        }
+        bingo_count = 0;
+        is_end = true;
+        printf("inin\n");
+        break;
     
+    }
 
     /* ---------------------------------------------------------------------------------------*/
 }
@@ -189,33 +218,33 @@ void ButtonUp(int face, int lednum)
 void pressButtonsAutomatically(Coroutine *coroutine) {
     BEGIN_COROUTINE(coroutine);
     ButtonDown(0, 4);
+    ButtonUp(0, 4);
     WAIT_FOR_MILISEC(coroutine, 4000);
     ButtonDown(0, 2);
+    ButtonUp(0, 2);
     WAIT_FOR_MILISEC(coroutine, 4000);
     ButtonDown(0, 0);
+    ButtonUp(0, 0);
     WAIT_FOR_MILISEC(coroutine, 4000);
     ButtonDown(0, 1);
+    ButtonUp(0, 1);
     WAIT_FOR_MILISEC(coroutine, 4000);
     ButtonDown(0, 3);
+    ButtonUp(0, 3);
     WAIT_FOR_MILISEC(coroutine, 4000);
     ButtonDown(0, 5);
+    ButtonUp(0, 5);
+    ButtonDown(1, 1);
     WAIT_FOR_MILISEC(coroutine, 4000);
     ButtonDown(0, 7);
+    ButtonUp(0, 7);
     WAIT_FOR_MILISEC(coroutine, 4000);
-    ButtonDown(1, 1);
+    ButtonDown(2, 1);
+    
+    
     // 마구잡이로 아무 버튼이나 클릭하는 무한루프...
 
-    while (bingo_count >= 3) {
-        for(int i=0; i<6; i++){
-            for(int j=0; j<9; j++){
-                if (current_bingo[i][j] == 1){
-                    ButtonDown(i, j);
-                    WAIT_FOR_MILISEC(coroutine, 4000);
-                }
-            }
-        }
     
-    }
 
     
 
